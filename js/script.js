3e3d463324c49b60395947e5e35ff1763de9309f
JoaoVTBorrachini <joaovthomazoni@gmail.com>
2023-11-23 16:13:19 -0300

    Alert GameOver

--- js/script.js
+++ js/script.js
@@ -15,22 +15,29 @@ const loop = setInterval(() => {
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-
+        mario.src = './images/game-over.png';
+        
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
 
-        mario.src = './images/game-over.png';
         mario.style.width = '70px'
         mario.style.marginLeft = '50px'
 
         clearInterval(loop)
-            alert( "Game over :(  |  Renicie a página para recomeçar" );
+        gameOver()
     }
+    
 
 }, 10);
+
+const gameOver = () => {
+    setTimeout(() => {
+        alert("Game over :( | Reinicie a página para recomeçar");
+    }, 250);
+}
 
 document.addEventListener('keydown', event => {
     if (event.code === 'Space') {
