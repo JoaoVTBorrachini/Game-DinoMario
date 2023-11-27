069a5e4d6b18cc7f27874d211769bcf0df5bbf96
JoaoVTBorrachini <joaovthomazoni@gmail.com>
2023-11-27 14:32:59 -0300

    Simple-Changes-clouds

--- js/script.js
+++ js/script.js
@@ -1,35 +1,48 @@
 const mario = document.querySelector('.mario');
+const clouds = document.querySelector('.clouds');
 const pipe = document.querySelector('.pipe');
+
+
+let pipeArray = [];
+let pipePos = [bottom = -20,
+                width = 80]
+
+
+// document.addEventListener("keydown", startScreen, {once: true})
 
 const jump = () => {
     mario.classList.add('jump');
 
-    setTimeout(()=> {
+    setTimeout(() => {
         mario.classList.remove('jump');
     }, 500)
 }
 
-const loop = setInterval(() => {
+const loop = setInterval(function () {
 
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const cloudsPosition = clouds.offsetLeft;
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         mario.src = './images/game-over.png';
-        
+
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
 
-        mario.style.width = '70px'
-        mario.style.marginLeft = '50px'
+        mario.style.width = '70px';
+        mario.style.marginLeft = '50px';
+
+        clouds.style.animation = 'none';
+        clouds.style.left = `${cloudsPosition}px`;
 
         clearInterval(loop)
         gameOver()
     }
-    
+
 
 }, 10);
 
@@ -41,6 +54,6 @@ const gameOver = () => {
 
 document.addEventListener('keydown', event => {
     if (event.code === 'Space') {
-      jump()
+        jump()
     }
-  });
+});
